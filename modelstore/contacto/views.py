d750aa3b3d327c6d650de3c6345903580209da5e
Frogy1961 <mantenimiento.total.proyectos@gmail.com>
2023-05-18 13:42:18 -0600

    Envio mensajes 180523

--- modelstore/contacto/views.py
+++ modelstore/contacto/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 
 def contact(request):
     formulario_contacto=FormularioContacto()
+
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+
+            email=EmailMessage("Mensaje de H&B Shop",
+            "De {} con email {} enía el siguiente mensaje:\n\n {}".format(nombre,email,contenido),"",
+            ["home.bussines.shop@gmail.com"],reply_to=[email])
+
+            try:
+                email.send()
+
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?invalido")
 
     return render(request, "Contacto/Contacto.html", {'MiFormulario':formulario_contacto})
--- modelstore/modelstore/settings.py
+++ modelstore/modelstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9usaeib_s@#*z5w+v_cd6e6xi$co*z9!cu!45s!smmr)rjl5$k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -140,3 +141,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
