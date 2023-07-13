4374133486fadfe42869ddb7e539e551bd517a5d
Frogy1961 <mantenimiento.total.proyectos@gmail.com>
2023-07-13 12:06:20 -0600

    Carro listo 130723

--- modelstore/carro/context_processor.py
+++ modelstore/carro/context_processor.py
@@ -2,8 +2,11 @@
 
 def importe_total_carro(request):
     total=0
-    #if request.user.is_authenticated:
-    '''for key, value in request.session["carro"].items():
-        total=total+float(value["precio"])'''
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total=total+float(value["precio"])
+
+    else:
+        total="No estas logeado"
 
     return {'importe_total_carro':total}
