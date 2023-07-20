1d0c90001334a34957671fc9bb991119f705c229
Frogy1961 <mantenimiento.total.proyectos@gmail.com>
2023-07-20 11:19:59 -0600

    Boton compra 200723

--- modelstore/pedidos/admin.py
+++ modelstore/pedidos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pedido, LineaPedido
 
 # Register your models here.
+
+class CategoriaProdAdmin(admin.ModelAdmin):
+    readonly_fields=('created')
+
+admin.site.register([Pedido, LineaPedido])
